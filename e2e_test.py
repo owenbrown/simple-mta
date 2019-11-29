ec89f45bbf677ddaa2f0850f0ce2708211cd0517
Owen Brown <owen.brown.aws@gmail.com>
2019-11-29 07:13:23 -0800

    tweak e_2_e testing

--- e2e_test.py
+++ e2e_test.py
@@ -3,7 +3,7 @@ import urllib.request
 HOST = 'http://simple-mta.appspot.com'
 
 # [START e2e]
-response = urllib.request.urlopen("/")
+response = urllib.request.urlopen(HOST + "/")
 html = response.read()
 assert(html == "Tranquily Base")
 # [END e2e]
